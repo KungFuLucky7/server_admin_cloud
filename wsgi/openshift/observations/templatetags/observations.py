6fbf4adcb731f8f551e9eb6404e1c99f6416670c
Terry Chun Wong <terrywong283@live.com>
2014-11-03 22:45:14 -0800

    Added 'Return' button and keeping the query string for list view and form view

--- wsgi/openshift/observations/templatetags/observations.py
+++ wsgi/openshift/observations/templatetags/observations.py
@@ -1,5 +1,6 @@
 from django import template
 from django.template.defaultfilters import stringfilter
+import urllib2
 
 register = template.Library()
 
@@ -16,3 +17,32 @@ def list_get_app_name(value):
     """Get the app name in the list view"""
     url_parts = value.rsplit('/', 2)
     return url_parts[1]
+    
+@register.filter
+@stringfilter
+def trim_query_string(value):
+    """In list view, Quick Start Guide, trim the query string to the correct format"""
+    query_string = value
+    if 'e=guide&' in value:
+        query_string = value.replace('e=guide&', '')
+    elif 'e=guide' in value:
+        query_string = value.replace('e=guide', '')
+    return query_string
+    
+@register.filter
+@stringfilter
+def trim_url(value):
+    """Trim url for the correct path"""
+    url_parts = value.split('/', 4)
+    url_parts.remove(url_parts[4])
+    url = '/'.join(url_parts)
+    return url
+    
+@register.filter
+@stringfilter
+def modify_query_string(value):
+    """In form view, modify the query string to the correct format"""
+    query_string = value
+    #if '_changelist_filters=' in value:
+        #query_string = value.replace('_changelist_filters=', '')
+    return urllib2.unquote(query_string)
